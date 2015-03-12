7db9c402c62e62d8bf4c6617851dc1cfd4f08214
Kiril Seksenov <seksenov@outlook.com>
2015-03-11 18:57:21 -0700

    adding the winRT component

---
+++ TostWinRT/ToastClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Streams;
+using Windows.Foundation;
+
+namespace TostWinRT
+{
+    [Windows.Foundation.Metadata.AllowForWeb]
+    public sealed class ToastClass
+    {
+        public async void toastMessage(String message, int delay)
+        {
+            ToastTemplateType toastTemplate = ToastTemplateType.ToastImageAndText01;
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+
+            XmlNodeList toastTextElements = toastXml.GetElementsByTagName("text");
+            toastTextElements[0].AppendChild(toastXml.CreateTextNode(message));
+
+            XmlNodeList toastImageAttributes = toastXml.GetElementsByTagName("image");
+            //((XmlElement)toastImageAttributes[0]).SetAttribute("src", "http://microsoft-chat.com/wp-content/uploads/2014/05/Microsoft_0-300x168.jpg");
+            //((XmlElement)toastImageAttributes[0]).SetAttribute("src", "https://raw.githubusercontent.com/seksenov/grouppost/master/images/AddNote.png");
+            ((XmlElement)toastImageAttributes[0]).SetAttribute("src", "https://raw.githubusercontent.com/seksenov/grouppost/master/images/logo.png");
+            ((XmlElement)toastImageAttributes[0]).SetAttribute("alt", "red graphic");
+
+            IXmlNode toastNode = toastXml.SelectSingleNode("/toast");
+            ((XmlElement)toastNode).SetAttribute("duration", "short");
+
+            XmlElement audio = toastXml.CreateElement("audio");
+            audio.SetAttribute("src", "ms-winsoundevent:Notification.IM");
+            toastNode.AppendChild(audio);
+
+            ((XmlElement)toastNode).SetAttribute("launch", "{\"type\":\"toast\",\"param1\":\"12345\",\"param2\":\"67890\"}");
+
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            await Task.Delay(delay);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+    }
+
+   
+}
