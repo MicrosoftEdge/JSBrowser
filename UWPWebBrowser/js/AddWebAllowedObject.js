cb6f1865af8ae99aa4a18ab308cccf4b88107253
Kiril Seksenov <seksenovk@gmail.com>
2015-07-16 10:11:43 -0700

    more updates

---
+++ UWPWebBrowser/js/AddWebAllowedObject.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener("DOMContentLoaded", ready, false)
+
+
+function ready() {
+    var webview = document.getElementById("WebView");
+
+    //var communicationWinRT = new CommunicationWinRT.CommunicationWinRT();
+    var communicationWinRT = new ToastWinRT.ToastClass();
+    //var cameraWinRT = new CommunicationWinRT.cameraWinRT();
+    webview.addEventListener("MSWebViewNavigationStarting", navigationStart);
+    //webview.addApplicationObject("CommunicatorWinRT", communicationWinRT);
+    //webview.addApplicationObject("cameraWinRT", cameraWinRT);
+    webview.addWebAllowedObject("CommunicatorWinRT", communicationWinRT);
+    //webview.addWebAllowedObject("cameraWinRT", cameraWinRT);
+    webview.navigate("ms-appx-web:///pages/page.html");
+
+}
+
+function navigationStart() {
+
+}
---
+++ UWPWebBrowser/js/page.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DomContentLoaded", function (e) { 
+    var notify_btn = document.getElementById("notify_btn");
+    notify_btn.addEventListener("click", notify, false);
+}
+, false);
+
+
+    function notify() {
+        var object = window.CommunicatorWinRT;
+
+        if (object) {
+            object.toastMessage('Hello World', 0);
+        } else {
+            console.log('Error injected object not found');
+        }
+
+    }
