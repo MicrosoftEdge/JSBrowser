43cd67aa9481270f2d3eee592a8794b629d46a3c
joshrenn <joshrenn@microsoft.com>
2015-08-11 10:45:38 -0700

    Remove unnecessary files

--- UWPWebBrowser/js/AddWebAllowedObject.js
+++
@@ -1,7 +0,0 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
-    "use strict";
-    
-    let webview = document.querySelector("#WebView");
-    webview.addWebAllowedObject("CommunicatorWinRT", new ToastWinRT.ToastClass);
-    webview.navigate("ms-appx-web:///pages/page.html");
-});
